1fb6e4efe2d68de945e06a47606c00a325dc765b
andrey <maltsevandreywork@gmail.com>
2025-11-22 16:34:55 +0300

    Дата: 22.11.2025
    - Подключены самые главные зависимости
    - Созданы главные таблицы в базе данных
    - Созданы сущности для базы данных
    - Созданы enum для сущностей
    - Создан docker-compose файл
    - был создан миграционный файл

--- src/main/java/andrey/dev/worktelegrambot/enums/WorkSchedule.java
+++ src/main/java/andrey/dev/worktelegrambot/enums/WorkSchedule.java
@@ -3,14 +3,14 @@ import lombok.Getter;
 
 @Getter
 public enum WorkSchedule {
-//    FULL_TIME("full_time", "полная занятость ")
-//    , PART_TIME("part_time", "частичная занятость")
-//    , FLEXIBLE("flexible", "гибкий график");
-//    private final String name;
-//    private final String nameIInRussian;
-//
-//    WorkSchedule(String name, String nameIInRussian) {
-//        this.name = name;
-//        this.nameIInRussian = nameIInRussian;
-//    }
+    FULL_TIME("full_time", "полная занятость ")
+    , PART_TIME("part_time", "частичная занятость")
+    , FLEXIBLE("flexible", "гибкий график");
+    private final String name;
+    private final String nameIInRussian;
+
+    WorkSchedule(String name, String nameIInRussian) {
+        this.name = name;
+        this.nameIInRussian = nameIInRussian;
+    }
 }
